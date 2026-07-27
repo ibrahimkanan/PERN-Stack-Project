12ca2431d3236aeabe2acd80fa502da988dacf40
ibrahimkanan <ibrahimkanan20@gmail.com>
2026-07-27 20:36:57 +0300

    basic routes setup

---
+++ backend/controllers/productController.js
@@ -0,0 +1,8 @@
+export const getAllProducts = async (req, res) => {
+    console.log("Get all products");
+};
+
+export const createProduct = async (req, res) => {
+    console.log("Create product");
+};
+ 
