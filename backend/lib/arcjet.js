21de2e5380b1c56faf4a84f7e30c935493a044ff
ibrahimkanan <ibrahimkanan20@gmail.com>
2026-08-01 17:51:35 +0300

    fix : fix bug in rate limiting

--- backend/lib/arcjet.js
+++ backend/lib/arcjet.js
@@ -4,13 +4,14 @@ import "dotenv/config";
 
 // init arcjet
 
-export const aj = arcjet(process.env.ARCJET_KEY, {
+export const aj = arcjet({
+    key: process.env.ARCJET_KEY,
     characteristics: ["ip.src"],
     rules: [
         shield({ mode: "LIVE" }),
         detectBot({
             mode: "LIVE",
-            allow: ["CATEGORY: SEARCH_ENGINE", "CATEGORY: MONITORING"],
+            allow: ["CATEGORY:SEARCH_ENGINE", "CATEGORY:MONITORING"],
         }),
 
         // rate limiting
--- backend/middleware/rateLimit.js
+++ backend/middleware/rateLimit.js
@@ -1,17 +1,17 @@
-import  aj  from "@arcjet/node";
+import { aj } from "../lib/arcjet.js";
 
 export default async function rateLimit(req, res, next) {
     try {
         const decision = await aj.protect(req, {
-            request: 1,
+            requested: 1,
         });
 
         if (decision.isDenied()) {
-            if (decision.isRateLimited()) {
+            if (decision.reason.isRateLimit()) {
                 return res.status(429).json({
                     error: "Rate limited",
                 });
-            } else if (decision.isBot()) {
+            } else if (decision.reason.isBot()) {
                 return res.status(403).json({
                     error: "Blocked by bot protection",
                 });
@@ -20,14 +20,12 @@ export default async function rateLimit(req, res, next) {
                     error: "Blocked by Arcjet",
                 });
             }
-
-            return;
         }
 
         // Check for spoofed bots
         if (
             decision.results.some(
-                (result) => result.isBot() && result.reason.isSpoof(),
+                (result) => result.reason.isBot && result.reason.isBot() && result.reason.isSpoof(),
             )
         ) {
             return res.status(403).json({
